12a2ef80f227bb900d09b6c22935b34119e2365a
Simon K <jackofspaces@gmail.com>
2023-11-23 11:20:06 +0000

    adding `v1` capabilities to restful api, adding `openAPI` doc gen capabilities

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	V1 = "/v1"
+)
+
 var uptime time.Time
 
 func init() {
@@ -15,11 +19,21 @@ func init() {
 }
 
 func main() {
-	engine := gin.Default()
-	engine.GET("/healthcheck", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/healthcheck", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"uptime": fmt.Sprintf("%d seconds", int(time.Since(uptime).Seconds())),
 		})
 	})
-	engine.Run()
+	registerVersionOne(router)
+	router.Run()
+}
+
+// Implement API version 1 capabilities.
+func registerVersionOne(router *gin.Engine) *gin.Engine {
+	apiV1 := router.Group(V1)
+	apiV1.GET("/envs", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "return env list later")
+	})
+	return router
 }
