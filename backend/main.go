d66348ee373d2f1a4907cab19fe167dce2276ad2
Simon K <jackofspaces@gmail.com>
2023-11-23 10:58:38 +0000

    adding `/healthcheck`

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+var uptime time.Time
+
+func init() {
+	uptime = time.Now()
+}
+
 func main() {
 	engine := gin.Default()
-	engine.GET("/ping", func(c *gin.Context) {
+	engine.GET("/healthcheck", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"uptime": fmt.Sprintf("%d seconds", int(time.Since(uptime).Seconds())),
 		})
 	})
 	engine.Run()
